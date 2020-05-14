a2928b03811aece2465b247415d625dce8f8634f
Joshua Tan <jtsarvida@gmail.com>
2020-05-14 11:14:41 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,15 @@
 class Post < ActiveRecord::Base
 
-  validate :is_title_case 
-  before_validation :make_title_case 
+  validate :is_title_case
+  before_validation :make_title_case
   belongs_to :author
 
   #put new code here
 
   private
+  def self.by_author(author_id)
+    where(author: author_id)
+  end
 
   def is_title_case
     if title.split.any?{|w|w[0].upcase != w[0]}
